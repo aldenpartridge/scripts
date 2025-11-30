b501da4a824d0126b7e6282d47e486e6429468f9
aldenpartridge <aldenpartridge@wearehackerone.com>
2025-11-29 22:44:34 -0500

    Modify script for random Mullvad server reconnection
    Updated script to reconnect to new Mullvad servers at a random time up to 12 hours, and added a list of locations.

--- rand-serv.sh
+++ rand-serv.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-#Script to make Mullvad randomly reconnect to new servers once per day at a random time.
+# Script to make Mullvad randomly reconnect to new servers at a random time up to 12hrs.
+# Albania al, Australia au, Austria at
+# Belgium be, Brazil br, Bulgaria bg
+# Canada ca, Chile cl, Colombia co
+# Czechia cz, Denmark dk, Estonia ee
+# Finland fi, France fr, Germany de
+# Hong Kong hk, Italy it, Japan jp
+# Mexico mx, Netherlands nl, Norway no
+# Peru pe, Poland pl, Romania ro
+# Singapore sg, Spain es, Sweden se
+# Switzerland ch, United Kingdom gb, United States us
+# Argentina ar
+
+LOCATIONS=(
+  "al" "au" "at"
+  "be" "br" "bg"
+  "ca" "cl" "co"
+  "cz" "dk" "ee"
+  "fi" "fr" "de"
+  "hk" "it" "jp"
+  "mx" "nl" "no"
+  "pe" "pl" "ro"
+  "sg" "es" "se"
+  "ch" "gb" "us"
+  "ar"
+)
+
+RANDLOC=${LOCATIONS[$RANDOM % ${#LOCATIONS[@]}]}
 
 RANDNUM=$(shuf -i 1-43200 -n 1)
 sleep $((RANDNUM))s
+
+mullvad relay set tunnel wireguard entry location us
+mullvad relay set location $RANDLOC
 mullvad reconnect
